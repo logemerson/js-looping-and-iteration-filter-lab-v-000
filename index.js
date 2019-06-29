07e182779e4028d7343ae15edeba72ebfca9b5dd
Logan Emerson <logemerson@gmail.com>
2019-06-29 14:29:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-function findMatching(drivers, string) {
-  return drivers.filter(function(driver) {
-    return driver.toUpperCase() === string.toUpperCase();
-  });
-}
-
-function fuzzyMatch(drivers, string) {
-  return drivers.filter(function(driver) {
-    return driver.startsWith(string);
-  });
-}
-
-function matchName(drivers, string) {
-  return drivers.filter(function(driver) {
-    return driver.name === string;
-  });
+function findMatching(drivers, name) {
+  function nameFilter(name){
+    return 
+  }
 }
